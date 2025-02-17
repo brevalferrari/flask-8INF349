6c1ea8e95e83cf90bce520b4bc34e41f23020b7c
p6nj <breee@duck.com>
2025-02-17 15:36:11 -0500

    get & put order

--- flaskstarter/routes/json_schemas.py
+++ flaskstarter/routes/json_schemas.py
@@ -1,7 +1,35 @@
+"""
+JSON schemas to validate sent form data.
+"""
+
 new_order = {
     "type": "object",
-    "properties":{
-        "id": {"type":"number"},
-        "quantity": {"type":"number"}
-    }
+    "properties": {"id": {"type": "number"}, "quantity": {"type": "number"}},
+}
+
+put_order_shipping_info = {
+    "type": "object",
+    "properties": {
+        "order": {
+            "email": {"type": "string"},
+            "shipping_information": {
+                "country": {"type": "string"},
+                "address": {"type": "string"},
+                "postal_code": {"type": "string"},
+            },
+        }
+    },
+}
+
+put_order_credit_card = {
+    "type": "object",
+    "properties": {
+        "credit_card": {
+            "name": {"type": "string"},
+            "number": {"type": "string"},
+            "expiration_year": {"type": "number"},
+            "cvv": {"type": "string"},
+            "expiration_month": {"type": "number"},
+        }
+    },
 }
