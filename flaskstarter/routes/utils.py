4fb28e5eb7d45ba11438ca7684b99c8301fe8dc4
p6nj <breee@duck.com>
2025-02-17 16:18:04 -0500

    split put_order, merge api utils with api

--- flaskstarter/routes/utils.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-from flask import Response
-
-def response_with_headers(body, status=200, **headers)->Response:
-    response = Response(body, status=status)
-    for k, val in headers.items():
-        response.headers[k] = val
-    return response
