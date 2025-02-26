bd2d0c89f5217a73903c5588bd41a99b2beae9ec
Breval Ferrari <breval.ferrari@fish.golf>
2025-02-26 11:44:19 -0500

    database secure reset

--- flaskstarter/app.py
+++ flaskstarter/app.py
@@ -11,14 +11,25 @@ __all__ = ["create_app"]
 
 
 def create_app(config=None):
-    db.connect()
-    with db.manual_commit() as _:
-        db.begin()
-        drop_products()
-        db.commit()
-    with db.manual_commit() as _:
-        db.begin()
-        for p in fetch_products():
-            add_product(id=p["id"], name=p["name"], in_stock=p["in_stock"], description=p["description"], price=p["price"], weight=p["weight"], image=p["image"])
-        db.commit()
+    try:
+        db.connect()
+        with db.manual_commit() as _:
+            db.begin()
+            drop_products()
+            db.commit()
+        with db.manual_commit() as _:
+            db.begin()
+            for p in fetch_products():
+                add_product(
+                    id=p["id"],
+                    name=p["name"],
+                    in_stock=p["in_stock"],
+                    description=p["description"],
+                    price=p["price"],
+                    weight=p["weight"],
+                    image=p["image"],
+                )
+            db.commit()
+    except:
+        pass
     return api
